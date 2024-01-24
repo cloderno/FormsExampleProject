212bd7ef96dc8a11283b5bcce512dadb43d5a335
Yeldar <cloderno@gmail.com>
2024-01-24 12:13:47 +0600

    Initial commit

---
+++ app/src/main/java/com/example/formsexampleproject/core/Status.kt
@@ -0,0 +1,5 @@
+package com.example.formsexampleproject.core
+
+enum class Status {
+    Available, Unavailable, Losing, Lost
+}
---
+++ app/src/main/java/com/example/formsexampleproject/presentation/main/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.formsexampleproject.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import com.example.formsexampleproject.R
+import com.example.formsexampleproject.core.ConnectivityObserver
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var connectivityObserver: ConnectivityObserver
+
+    val tv by lazy { findViewById<TextView>(R.id.text) }
+    val btnRu by lazy { findViewById<TextView>(R.id.btnRu) }
+    val btnKz by lazy { findViewById<TextView>(R.id.btnKz) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        connectivityObserver = ConnectivityObserver.Base(applicationContext)
+
+        lifecycleScope.launch {
+            connectivityObserver.observe().collect { status ->
+                tv.text = status.toString()
+            }
+        }
+
+        btnRu.setOnClickListener {
+
+        }
+
+        btnKz.setOnClickListener {
+
+        }
+    }
+}
